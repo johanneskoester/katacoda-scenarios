5a90ac61c7773fec22bf4c7576bf6d9779d8147b
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-06-20 19:02:06 +0200

    minor.

--- snakemake-intro/setup.sh
+++ snakemake-intro/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # setup environment
-docker run -i -t -v `pwd`:/workspace -w /workspace quay.io/snakemake/snakemake bash
+docker run -i -t -v `pwd`:/workspace -w /workspace quay.io/snakemake/snakemake:v5.1.4 bash
 # obtain data
 git clone https://bitbucket.org/snakemake/snakemake-tutorial.git
+cd snakemake-tutorial
 rm README.md
 rm environment.yaml
