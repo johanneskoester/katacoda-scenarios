86c5a2e667784d4858f60640c1f574a9f0f879d4
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-06-20 17:59:26 +0200

    Updated

--- snakemake-intro/setup.sh
+++ snakemake-intro/setup.sh
@@ -1,8 +1,13 @@
+
+echo Setup environment, this can take a few minutes...
+
 curl -L -o /tmp/miniconda.sh  https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash miniconda.sh -b -p /tmp/miniconda
+bash /tmp/miniconda.sh -b -p /tmp/miniconda
 export PATH=/tmp/miniconda/bin:$PATH
 
 conda config --system --add channels conda-forge
 conda config --system --add channels bioconda
 
 conda install snakemake
+
+echo Setup complete!
