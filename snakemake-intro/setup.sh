fedcc4c3c4581be6e2d2298bd500a73bb04bd413
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-06-20 18:36:24 +0200

    Updated

--- snakemake-intro/setup.sh
+++ snakemake-intro/setup.sh
@@ -1,15 +1,3 @@
+#!/bin/bash
 
-date
-echo Setup environment, this can take a few minutes...
-
-curl -L -o /tmp/miniconda.sh  https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash /tmp/miniconda.sh -b -p /tmp/miniconda
-export PATH=/tmp/miniconda/bin:$PATH
-
-conda config --system --add channels conda-forge
-conda config --system --add channels bioconda
-
-conda install -y snakemake
-
-echo Setup complete!
-date
+docker run -i -t -v `pwd`:/workspace -w /workspace quay.io/snakemake/snakemake bash
