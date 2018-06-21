f9cfa832b01aa31a9f7a78811a236d0b870f3f15
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-06-21 19:48:21 +0200

    remove superfluous conda installation

--- snakemake-intro/setup.sh
+++ snakemake-intro/setup.sh
@@ -2,7 +2,6 @@
 
 echo setup environment...
 docker run -i -t -v `pwd`:/workspace -w /workspace quay.io/snakemake/snakemake bash
-conda install -y -c conda-forge xorg-libxrender xorg-libxpm
 echo obtain data...
 git clone https://bitbucket.org/snakemake/snakemake-tutorial.git
 cd snakemake-tutorial
