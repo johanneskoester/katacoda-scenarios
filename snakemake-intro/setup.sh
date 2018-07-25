cce6e28639d191265e0e510c0ab57244edbcf150
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-07-25 18:04:48 +0200

    Use ubuntu image and docker again.

--- snakemake-intro/setup.sh
+++ snakemake-intro/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo setup environment...
-#docker run -i -t -v `pwd`:/workspace -w /workspace quay.io/snakemake/snakemake bash
-conda create -n snakemake snakemake-minimal networkx pygraphviz graphviz
-source activate snakemake
+docker run -i -t -v `pwd`:/workspace -w /workspace quay.io/snakemake/snakemake bash
+#conda create -n snakemake snakemake-minimal networkx pygraphviz graphviz
+#source activate snakemake
 echo obtain data...
 git clone https://bitbucket.org/snakemake/snakemake-tutorial.git
 cd snakemake-tutorial
